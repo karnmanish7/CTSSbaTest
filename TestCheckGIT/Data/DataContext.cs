ddb94ad37d3c1557d593dae1eb842000b772906f
github_Manish <m.karn2011@gmail.com>
2020-06-06 13:50:00 +0530

    PaymentDetails Added

---
+++ TestCheckGIT/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCheckGIT.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options){ }
+        
+    }
+}
---
+++ TestCheckGIT/Models/PaymentDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCheckGIT.Models
+{
+    public class PaymentDetails
+    {
+        public string CardHolderName { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string CardNumber { get; set; }
+        public int cvv { get; set; }
+    }
+}

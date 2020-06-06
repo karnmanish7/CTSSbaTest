900909f7149f7845deed9518955fb83d1906b87f
github_Manish <m.karn2011@gmail.com>
2020-06-06 14:12:57 +0530

    dbcontext and connection string added

--- TestCheckGIT/Data/DataContext.cs
+++ TestCheckGIT/Data/DataContext.cs
@@ -3,12 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestCheckGIT.Models;
 
 namespace TestCheckGIT.Data
 {
     public class DataContext:DbContext
     {
         public DataContext(DbContextOptions<DataContext>options):base(options){ }
-        
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
     }
 }
--- TestCheckGIT/Startup.cs
+++ TestCheckGIT/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TestCheckGIT.Data;
+using TestCheckGIT.Models;
 
 namespace TestCheckGIT
 {
@@ -26,11 +29,14 @@ namespace TestCheckGIT
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {          
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

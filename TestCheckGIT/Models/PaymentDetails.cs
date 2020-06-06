7341f5df84b2efc4ef088693a09f3dcdb0be004a
github_Manish <m.karn2011@gmail.com>
2020-06-06 16:33:30 +0530

    migration and connection details added

--- TestCheckGIT/Models/PaymentDetails.cs
+++ TestCheckGIT/Models/PaymentDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace TestCheckGIT.Models
 {
     public class PaymentDetails
     {
+        [Key]
+        public int PMID { get; set; }
         public string CardHolderName { get; set; }
         public DateTime ExpiryDate { get; set; }
         public string CardNumber { get; set; }
